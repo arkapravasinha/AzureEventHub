842e10a7aa0f835ab1b52f528507200a593de190
Arkaprava Sinha <arka@devilsdoom.in>
2020-08-17 21:38:59 +0530

    done

---
+++ AzureEventHub.Receiver/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureEventHub.Receiver
+{
+    class Program
+    {
+        private const string Storage_Account_Connection_String = "DefaultEndpointsProtocol=https;AccountName=eventhubhost;AccountKey=ZkrZhaDlAYnzfKdDZxTmcwbCdBn1EDUWshueZX803fK3uw3svOYHYBb/6OxTKw76I0kHbqXRnee3yLBgvilRUQ==;EndpointSuffix=core.windows.net";
+        private const string Storage_Container_Name = "hub1data";
+        private const string Event_Hub_Connection_string = "Endpoint=sb://arkaeventhubtest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=e6cfQ6SSLZbVKJXceLViilvPs4pQnc9fXxQOMoUapR8=";
+        private const string Event_Hub_Name = "hub1";
+        static void Main(string[] args)
+        {
+            MainAsyncTask().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsyncTask()
+        {
+            Console.WriteLine("Registering event processor");
+            var eventProcessorhost = new EventProcessorHost
+                (
+                Event_Hub_Name,
+                PartitionReceiver.DefaultConsumerGroupName,
+                Event_Hub_Connection_string,
+                Storage_Account_Connection_String,
+                Storage_Container_Name
+                );
+
+            await eventProcessorhost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving messages, press enter to stop the worker");
+            Console.ReadLine();
+
+            await eventProcessorhost.UnregisterEventProcessorAsync();
+        }
+    }
+}
--- AzureEventHub/Program.cs
+++ AzureEventHub/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Azure.EventHubs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,47 @@ namespace AzureEventHub
 {
     class Program
     {
+        private static EventHubClient eventHubClient;
+        private const string Event_Hub_Connection_string = "Endpoint=sb://arkaeventhubtest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=e6cfQ6SSLZbVKJXceLViilvPs4pQnc9fXxQOMoUapR8=";
+        private const string Event_Hub_Name = "hub1";
         static void Main(string[] args)
         {
+            MainAsyncTask().GetAwaiter().GetResult();
+            
+            Console.ReadKey();
+        }
+
+        static async Task MainAsyncTask()
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(Event_Hub_Connection_string)
+            {
+                EntityPath=Event_Hub_Name
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub();
+
+            if (!eventHubClient.IsClosed)
+                await eventHubClient.CloseAsync();
+        }
+
+        private async static Task SendMessagesToEventHub()
+        {
+            for (int i = 0; i < 100; i++)   
+            {
+                try
+                {
+                    var message = $"Message {i} at {DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss:ms")}";
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));    
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                await Task.Delay(10);
+            }
         }
     }
 }
